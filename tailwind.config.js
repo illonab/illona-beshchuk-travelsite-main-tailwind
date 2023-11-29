c7d3d65d8b92221dc8b39c7c4bde68f392f795ea
Ilona <lona.krechko@gmail.com>
2023-11-29 22:32:46 +0000

    Added recommendations section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./*.html"],
   theme: {
     extend: {
       colors: {
         travelBlue: "#335576",
-        travelOrange: "DB7A4E",
-        travelLight: "FEFAF5",
+        travelOrange: "#DB7A4E",
       },
     },
   },
